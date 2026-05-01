fd5e0b4e04f1781310ce537defd0513f07a32036
omar-mady-honeybadger <omar.mady2017@gmail.com>
2026-05-02 02:19:18 +0300

    new 1

--- lib/features/search/data/models/search_result.dart
+++ lib/features/search/data/models/search_result.dart
@@ -2,11 +2,13 @@ class SearchResult {
   final String url;
   final String title;
   final String content;
+  final int occurrenceCount;
 
   SearchResult({
     required this.url,
     required this.title,
     required this.content,
+    required this.occurrenceCount,
   });
 
   factory SearchResult.fromJson(Map<String, dynamic> json) {
@@ -14,6 +16,7 @@ class SearchResult {
       url: json['url'] ?? '',
       title: json['title'] ?? 'No Title',
       content: json['content'] ?? '',
+      occurrenceCount: json['occurrenceCount'] ?? 0,
     );
   }
 }
